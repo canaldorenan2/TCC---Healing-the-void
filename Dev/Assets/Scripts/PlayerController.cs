592ef491113af7c6d468b333799a9d3bdfe1677e
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2020-11-27 19:05:09 -0300

    Atualização na movimentação e conclusão da Camera
    Camera em terceira pessoa configurada para a fase 2;
    
    Inicio da animação

--- Dev/Assets/Scripts/PlayerController.cs
+++ Dev/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Não deixa o codigo rodar sem o animator
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
 
@@ -9,21 +11,111 @@ public class PlayerController : MonoBehaviour
 
     Animator animacaoPlayer;
 
+    Transform transformPlayer;
+
+    float axiX, axiY;
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidbodyPlayer = GetComponent<Rigidbody>();
 
-        animacaoPlayer = GetComponent<Animator>();
+
+        rigidbodyPlayer = transform.GetChild(0).GetComponent<Rigidbody>();
+
+        animacaoPlayer = transform.GetChild(0).GetComponent<Animator>();
+
+
+        transformPlayer = GetComponent<Transform>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("w"))
+        axiX = Input.GetAxis("Horizontal");
+        axiY = Input.GetAxis("Vertical");
+
+        Movimentacao();
+
+    }
+
+
+
+    void Movimentacao()
+    {
+
+        
+        if (axiX != 0)
+        {
+            transform.Rotate(0, axiX * 10 * Time.deltaTime, 0);
+        }
+
+      
+        // Frente
+
+        
+        if (Input.GetKey("w"))
+        {
+
+            animacaoPlayer.SetFloat("run", axiY);
+            rigidbodyPlayer.AddForce(0, 100 * Time.deltaTime, 0);
+        }else
+        {
+            animacaoPlayer.SetFloat("run", axiY);
+        }
+
+        /*
+        // Vira para esquerda
+        if (Input.GetKeyDown("a"))
         {
             animacaoPlayer.SetTrigger("run");
-            rigidbodyPlayer.AddForce(0,1,0);
+            transform.Rotate(0, -05, 0);
+            rigidbodyPlayer.AddForce(0, 0.80f, 0);
         }
+
+        // Vira para trás
+        if (Input.GetKeyDown("s"))
+        {
+            animacaoPlayer.SetTrigger("run");
+            transform.Rotate(0, -45, 0);
+            rigidbodyPlayer.AddForce(0, 0.80f, 0);
+        }
+
+        // Move para direita
+        if (Input.GetKeyDown("d"))
+        {
+            animacaoPlayer.SetTrigger("run");
+            transform.Rotate(0, -5, 0);
+            rigidbodyPlayer.AddForce(0, 0.80f, 0);
+        }
+        */
+
+        // Pula
+        if (Input.GetKeyDown("space"))
+        {
+            animacaoPlayer.SetBool("jump", true);
+            rigidbodyPlayer.AddForce(0, 0, 10);
+        }
+        else
+        {
+            animacaoPlayer.SetBool("jump", false);
+        }
+
+        // Ataque 1
+        if (Input.GetMouseButton(0))
+        {
+            animacaoPlayer.SetBool("slash1", true);
+            rigidbodyPlayer.AddForce(0, 0.1f, 0);
+        }
+        else
+        {
+            animacaoPlayer.SetBool("slash1", false);
+        }
+
+        // Verifica se está colidindo com o chão
+        //if (rigidbodyPlayer.transform.)
+
+        
     }
 }
---
+++ Dev/Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+
+    public float speed = 6;
+
+    float horizontal, vertical, targetAngle;
+
+    public Transform camera;
+
+    Vector3 moveDirection;
+
+    Vector3 direction;
+
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+        direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        // Verifica o tamanho do vector
+        if (direction.magnitude > 0.1f)
+        {
+            // Aqui ele vai pegar a tangente de x sobre y, resultando em um radiano anti horário
+            targetAngle = Mathf.Atan2(direction.x, direction.z);
+
+            // Transforma o valor de radianos para graus
+            targetAngle = targetAngle * Mathf.Rad2Deg;
+
+            // Pega a rotação de Y
+            targetAngle = targetAngle + camera.eulerAngles.y;
+
+            // Aplica a rotação 
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            // Converte a rotação em direção
+            moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            // Realiza o movimento 
+            characterController.Move(moveDirection * speed * Time.deltaTime);
+        }
+
+        ControleAnimacao();
+    }
+
+    void ControleAnimacao()
+    {
+        if (direction.magnitude > 0.1f)
+        {
+            animator.SetBool("run", true);
+        }
+        else
+        {
+            animator.SetBool("run", false);
+        }
+    }
+}
