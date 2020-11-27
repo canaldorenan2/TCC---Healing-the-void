40956c660a61357c4837107f05601d0b8b9f3ed9
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2020-11-27 07:36:06 -0300

    Commit de segurança

--- Dev/Assets/Scripts/PlayerController.cs
+++ Dev/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public Rigidbody rigidBody;
+    Rigidbody rigidbodyPlayer;
 
+    Animator animacaoPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbodyPlayer = GetComponent<Rigidbody>();
+
+        animacaoPlayer = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("w"))
+        {
+            animacaoPlayer.SetTrigger("run");
+            rigidbodyPlayer.AddForce(0,1,0);
+        }
     }
 }
