e90fa47140374883ed5dda3ae2379cabb5850f89
Renan Cascarano de Souza <30605188+canaldorenan2@users.noreply.github.com>
2021-03-06 19:51:14 -0300

    Comitoncios pt3

---
+++ Dev/Assets/Scripts/bossset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossset : MonoBehaviour
+
+{
+
+    public Terrain terreno;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            terreno.gameObject.SetActive(false);
+            other.gameObject.transform.Translate(10 * Time.deltaTime, 1 * Time.deltaTime, 10 * Time.deltaTime);
+            Destroy(this, 2);
+        }
+    }
+}
